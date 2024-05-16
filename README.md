a07159e162454ba5c44cdfa79fc1ba4dea02be65
Lucas Rafaldini Couto Silva <lucas.rafaldini@mercadolivre.com>
2024-05-16 11:37:37 -0300

    Update README

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # poc-xbz
+
+
+To run on WIndows, just open the file `main.py` and add cnpj and xbz token.
+
+
+After that, you can run the file `main.py` directly or, if you are using Windows, you can run our bat file and make a simple starting configuration isolating the Python version that will run the script.
+
+In case you run the script directly, be sure to install the dependencies by running the command `pip install -r requirements.txt` in the terminal.
--- run_main.bat
+++ run_main.bat
@@ -1,14 +1,14 @@
 @echo off
 
 :: Isolate the python environment
-py -m pip install --upgrade pip
-py -m pip install virtualenv
+python -m pip install --upgrade pip
+python -m pip install --user virtualenv
 
 :: Create the virtual environment
-py -m virtualenv venv
+python -m virtualenv venv
 
 :: Activate the virtual environment
 call venv\Scripts\activate
 
 :: Install the required packages
-py -m pip install -r requirements.txt
+python -m pip install -r requirements.txt
